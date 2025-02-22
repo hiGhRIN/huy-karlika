43053f2ddc9e22605630d74ee63eb75107004193
GRIN <154383196+hiGhRIN@users.noreply.github.com>
2025-02-22 13:06:55 +0300

    Add files via upload

---
+++ пизда 1 егэ инфа/инцест номер 12.py
@@ -0,0 +1,21 @@
+print('номер 1.....☠️')
+s = '5' * 874
+while '555' in s or '111' in s:
+    if '555' in s:
+        s = s.replace('555', '1', 1)
+    else:
+        s = s.replace('111', '5', 1)
+print (s)
+
+print('номер 2...☠️☠️☠️☠️☠️☠️☠️')
+s = '1' + '0' * 55
+while '1' in s:
+    if '10' in s:
+        s = s.replace('10', '001', 1)
+    else:
+        s = s.replace('1', '00', 1)
+print (s)
+
+print('номер 3...☠️')
+s = '1' + '0' * 55
+while
